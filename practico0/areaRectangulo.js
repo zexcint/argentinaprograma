ef274531c9bdd556e4c688722d5c831189626d0e
Alcaudon <Alcaudon@mx>
2023-05-31 03:47:50 -0300

    Practico NO finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,7 @@
+// Se declaran dos variables, "area1" y "area2", que calculan el área de un cuadrado utilizando la fórmula base por altura.
+let area1 = 5 * 3;
+let area2 = 12.5 * 5.3;
+
+// Se imprime en la consola los valores de "area1" y "area2", que representan el área de los respectivos cuadrados calculados.
+console.log(area1);
+console.log(area2);
---
+++ practico0/promedio3.js
@@ -0,0 +1,13 @@
+// Se declaran tres variables, "promedio_1", "promedio_2" y "promedio_3", para almacenar los promedios de cada conjunto de valores.
+// Cada promedio se obtiene sumando todos los valores en un conjunto y luego dividiendo esa suma entre la cantidad de valores en ese conjunto.
+
+let promedio_1 = (5 + 7 + 9) / 3;
+
+let promedio_2 = (-2 + -3 + -8) / 3;
+
+let promedio_3 = (-1.2 + 5.3 + -3.3) / 3;
+
+// Se imprimen los valores de los promedios de cada variable en la consola.
+console.log(promedio_1);
+console.log(promedio_2);
+console.log(promedio_3);
