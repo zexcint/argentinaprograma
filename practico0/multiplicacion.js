4dee0733a7ef6b78fe46562ff6faf99dbdaa09d6
Alcaudon <Alcaudon@mx>
2023-05-31 03:46:21 -0300

    Agrego los archivos resta.js y multiplicacion.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+// Se declara una variable llamada "multiplicando", que almacena el resultado de la multiplicación entre 2, 17 y 6.
+let multiplicando = 2 * 17 * 6;
+
+// Se imprime en la consola el valor de la variable "multiplicando", que representa el resultado de la multiplicación de los 3 números.
+console.log(multiplicando);
---
+++ practico0/resta.js
@@ -0,0 +1,5 @@
+// Se declara una variable llamada "restando", que resta los valores 10 menos 2.
+let restando = 10 - 2;
+
+// Se imprime la variable "restando" en la consola, el valor será el resultado entre la resta de 10 menos 2.
+console.log(restando);
