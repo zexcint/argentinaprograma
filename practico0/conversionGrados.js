0c3b917ddc83ecd6c2beeec713e4594778cfea2b
Alcaudon <Alcaudon@mx>
2023-05-31 03:47:08 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,12 @@
+// Se declaran dos variables, "fahrenheit_1" y "fahrenheit_2", que representan los valores en grados Fahrenheit.
+let fahrenheit_1 = 50;
+let fahrenheit_2 = 75;
+
+// Se declaran dos variables, "celsius_1" y "celsius_2", para almacenar la conversión de grados Fahrenheit a grados Celsius.
+// La fórmula utilizada es: (Fahrenheit - 32) * 5/9.
+let celsius_1 = (fahrenheit_1 - 32) * 5/9;
+let celsius_2 = (fahrenheit_2 - 32) * 5/9;
+
+// Se imprime en la consola los valores de "celsius_1" y "celsius_2", que representan los grados Fahrenheit convertidos a Celsius.
+console.log(celsius_1);
+console.log(celsius_2);
