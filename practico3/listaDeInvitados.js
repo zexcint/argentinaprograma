0e661f4046bd607a7ddfe9fba5712eada0e09a33
Alcaudon <Alcaudon@mx>
2023-06-21 20:08:32 -0300

    Agregada carpeta practico3

---
+++ practico3/listaDeInvitados.js
@@ -0,0 +1,33 @@
+let personas = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro", "Emilia"];
+let admitidos = [];
+let rechazados = [];
+
+for (let i = 0; i < personas.length; i++) {
+  if (personas[i] === "Jose" || personas[i] === "Sofia") {
+    rechazados.push(personas[i]);
+  } else {
+    admitidos.push(personas[i]);
+  }
+}
+
+console.log("La lista de invitados admitidos es:");
+for (let i = 0; i < admitidos.length; i++) {
+  console.log(admitidos[i]);
+}
+
+console.log("La lista de invitados rechazados es:");
+for (let i = 0; i < rechazados.length; i++) {
+  console.log(rechazados[i]);
+}
+
+admitidos.sort();
+console.log("La lista ordenada de invitados admitidos es:");
+for (let i = 0; i < admitidos.length; i++) {
+  console.log(admitidos[i]);
+}
+
+rechazados.sort();
+console.log("La lista ordenada de invitados rechazados es:");
+for (let i = 0; i < rechazados.length; i++) {
+  console.log(rechazados[i]);
+}
