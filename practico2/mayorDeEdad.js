8f8618a57d1e82b5adf5bbf945260d4d9d6cc7f0
zexcint <134021276+zexcint@users.noreply.github.com>
2023-06-21 17:11:18 -0300

    Delete practico2 directory

--- practico2/mayorDeEdad.js
+++
@@ -1,8 +0,0 @@
-const readlineSync = require("readline-sync");
-const edad = parseInt(readlineSync.question("Ingrese su Edad: "));
-
-if (edad >= 18) {
-  console.log("Eres mayor de edad");
-} else {
-  console.log("Eres menor de edad");
-}
