a48d8801537b4c6a720abdaef079ae818512a336
Alcaudon <Alcaudon@mx>
2023-06-27 20:58:25 -0300

    V1

---
+++ extras/extra-junio-27.js
@@ -0,0 +1,22 @@
+/*
+1) Programar una función que tome un numero y que devuelva "Es par" si el número es par, y "Es impar" si el número es impar
+*/
+
+function calcularParImpar(numero) {
+  if (numero % 2 === 0) {
+    return 'Es par';
+  } else if (numero % 2 !== 0) {
+    return 'Es impar';
+  }
+}
+console.log(calcularParImpar(15));
+
+
+/*
+2) Programar una función que tome una frase y devuelva la cantidad de palabras que tiene la frase
+*/
+
+function longitudDeLaFrase(cadena) {
+  return cadena.length;
+}
+console.log(longitudDeLaFrase('Hola Mundo'));
